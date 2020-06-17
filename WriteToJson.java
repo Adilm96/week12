6c9b8bb3a047319b2515f6c44b7e0cc9b20a7828
Adilm96 <55747931+Adilm96@users.noreply.github.com>
2020-06-18 01:11:06 +0200

    Add files via upload

---
+++ WriteToJson.java
@@ -0,0 +1,45 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToJson {
+    @SuppressWarnings("unchecked")
+    public static void main( String[] args )
+    {
+        //First Employee
+        JSONObject countryDetails = new JSONObject();
+        countryDetails.put("country", "Denmark");
+        countryDetails.put("year", "2000");
+        countryDetails.put("total population", "2000000");
+
+        JSONObject countryObject = new JSONObject();
+        countryObject.put("country", countryDetails);
+
+        //Second Employee
+        JSONObject countryDetails2 = new JSONObject();
+        countryDetails2.put("country", "india");
+        countryDetails2.put("year", "2000");
+        countryDetails2.put("total population", "10000000");
+
+        JSONObject countryObject2 = new JSONObject();
+        countryObject2.put("country", countryDetails2);
+
+        //Add employees to list
+        JSONArray countryList = new JSONArray();
+        countryList.add(countryObject);
+        countryList.add(countryObject2);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("\\Users\\Adil\\Desktop\\employees.json")) {
+
+            file.write(countryList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
